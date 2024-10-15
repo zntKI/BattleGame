e080be6308df14466e76a33859a70c331144da3c
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2024-10-15 17:45:27 +0200

    Added and implemented base scene class
    - Also reorganized source files

---
+++ BattleGame/scene.cpp
@@ -0,0 +1,34 @@
+#include "scene.hpp"
+
+Scene::Scene( const std::string identifier )
+	: identifier( identifier )
+{
+}
+
+Scene::~Scene()
+{
+}
+
+void Scene::addGameObject( GameObject& gameObject )
+{
+	gameObjects.push_back( &gameObject );
+}
+
+void Scene::update()
+{
+	for ( unsigned int i = 0; i < gameObjects.size(); i++ ) {
+		gameObjects[ i ]->update();
+	}
+}
+
+void Scene::render( const sf::RenderWindow& window )
+{
+	for ( unsigned int i = 0; i < gameObjects.size(); i++ ) {
+		gameObjects[ i ]->render( window );
+	}
+}
+
+std::string Scene::getIdentifier() const
+{
+	return this->identifier;
+}
---
+++ BattleGame/scene.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+#include <SFML/Graphics.hpp>
+
+#include "gameObject.hpp";
+
+class Scene
+{
+private:
+	const std::string identifier;
+	std::vector<GameObject*> gameObjects;
+
+public:
+	Scene( const std::string identifier );
+	~Scene();
+
+public:
+	void addGameObject( GameObject& gameObject );
+	void update();
+	void render( const sf::RenderWindow& window );
+	std::string getIdentifier() const;
+};
